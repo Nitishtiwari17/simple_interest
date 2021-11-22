f0415097cab24c2550653286c45122e60d549ea5
Nitish Kumar Tiwari <nitishtiwari1710@gmail.com>
2021-11-23 00:09:57 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+            float p,r,t,si;
+            p=13000;
+            r=12;
+            t=2;
+            si=(p*r*t)/100;
+            System.out.println(si);
+        }
+
+    }
+
+        // write your code here
+
+
